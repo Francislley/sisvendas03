d06ffc6a334f46b5e317242a4b1570046a0f1f72
k3n3dfelix <kened.felix@gmail.com>
2014-11-19 15:03:50 -0200

    views
    criando funcao index

--- projeto/pessoas/views.py
+++ projeto/pessoas/views.py
@@ -3,3 +3,6 @@ from pessoas.forms import PessoaForm, LoginForm
 from pessoas.models import Pessoa
 from django.contrib.auth import authenticate, logout, login as meu_login
 from django.contrib.auth.decorators import login_required
+
+def index(request):
+	return render(request, 'index.html')
