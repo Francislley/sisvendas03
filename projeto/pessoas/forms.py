2ac8faded4f034245d098ab256f2c13a239ab8a2
k3n3dfelix <kened.felix@gmail.com>
2014-11-19 14:55:51 -0200

    forms
    criando login

--- projeto/pessoas/forms.py
+++ projeto/pessoas/forms.py
@@ -4,3 +4,7 @@ from pessoas.models import Pessoa
 class PessoaForm(forms.ModelForm):
 	class Meta:
 		model = Pessoa
+
+class LoginForm(forms.Form):
+    login = forms.CharField(max_length=100, required=True)
+    senha = forms.CharField(widget=forms.PasswordInput, required=True)
