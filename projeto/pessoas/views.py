a473ae86124f0d3336e4982871716d5aefa9f787
Diogo Rafael Braguim <dirafa@live.com>
2014-11-19 16:18:23 -0200

    views
    funcao validarlogin

--- projeto/pessoas/views.py
+++ projeto/pessoas/views.py
@@ -36,3 +36,22 @@ def logoff(request):
 	logout(request)
 	return HttpResponseRedirect('/')
 
+def validarlogin(request):
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+
+		if form.is_valid():
+			pessoa = authenticate(username=form.data['login'], password=form.data['senha'])
+
+			if pessoa is not None:
+				if pessoa.is_active:
+					meu_login(request, pessoa)
+					return HttpResponseRedirect('/')
+				else:
+					return render(request, 'login.html', {'form': form})
+			else:
+				return render(request, 'login.html', {'form': form})
+		else:
+			return render(request, 'login.html', {'form': form})
+	else:
+		return HttpResponseRedirect('/login/')
